94a4f30c3838a9105a25727d76b66e18630ff941
jatinthakur <jtkalga@gmail.com>
2022-07-01 08:58:42 +0530

    Added GeoLocation

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
+    <div v-if="!location">
+      {{ locErr }}
+    </div>
     <l-map
+      v-if="location"
       v-model="zoom"
       v-model:zoom="zoom"
-      :center="[31.995809, 77.450126]"
+      :center="currLocation"
       @zoom="zoomUpperBound"
       :options="{ zoomControls: false }"
     >
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       ></l-tile-layer>
-      <l-marker :lat-lng="[31.995809, 77.450126]" @moveend="check">
-        <l-tooltip> Sunrise Restaurant Kalga </l-tooltip>
+      <l-marker :lat-lng="currLocation" @moveend="check">
+        <l-tooltip> You </l-tooltip>
       </l-marker>
       <l-polyline
         :lat-lngs="[
@@ -40,6 +44,9 @@ export default {
     return {
       zoom: 2,
       geojson: null,
+      currLocation: [31.995809, 77.450126],
+      locErr: null,
+      location: false,
     };
   },
   components: {
@@ -58,6 +65,25 @@ export default {
         this.zoom = 18;
       }
     },
+  },
+  created() {
+    if (!("geolocation" in navigator)) {
+      this.locErr = "GeoLocation is not available";
+      this.location = false;
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        this.zoom = 18;
+        this.currLocation = [pos.coords.latitude, pos.coords.longitude];
+        this.location = true;
+        console.log(this.currLocation);
+      },
+      (err) => {
+        this.location = false;
+        this.locErr = err.message;
+      }
+    );
   },
 };
 </script>
