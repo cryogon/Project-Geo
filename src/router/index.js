352d8498f118cd8ced43d281d381034d4371f389
jatinthakur <jtkalga@gmail.com>
2022-07-06 07:57:32 +0530

    Fixed Gif not being pushed into repo

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,7 @@ const routes = [
   {
     path: "/map",
     name: "map",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/MapView.vue"),
+    component: () => import("../views/MapView.vue"),
     meta: {
       loginRequired: true,
     },
